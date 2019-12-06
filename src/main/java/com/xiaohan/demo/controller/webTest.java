6693c4003163d7113ebd373a35ac6ff3b8af98c8
lvmaizi <fwh17839936646>
2019-12-06 21:33:50 +0800

    init project

---
+++ src/main/java/com/xiaohan/demo/controller/webTest.java
@@ -0,0 +1,19 @@
+package com.xiaohan.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.Max;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class webTest {
+    @RequestMapping("test")
+    public String test(){
+        List<String> list = new ArrayList<>();
+        list.add("123");
+        list.add("456");
+        return "test";
+    }
+}
---
+++ src/test/java/com/xiaohan/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaohan.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
